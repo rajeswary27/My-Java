2aa27c9c3ecdc89c5504823003d71f317e0637c4
rajeswary27 <rajeswarynair68793@gmail.com>
2024-04-29 20:02:38 +0530

    updated

--- Maven_Project/src/main/java/excelread/Excel_Main.java
+++ Maven_Project/src/main/java/excelread/Excel_Main.java
@@ -7,6 +7,12 @@ public class Excel_Main {
 	public static void main(String[] args) throws IOException {
 		System.out.println(ExcelCode.getStringData(1,0));
 		System.out.println(ExcelCode.getIntegerData(1, 1));
+		System.out.println(ExcelCode.getStringData(2,0));
+		System.out.println(ExcelCode.getIntegerData(2, 1));
+		System.out.println(ExcelCode.getStringData(3,0));
+		System.out.println(ExcelCode.getIntegerData(3, 1));
+		System.out.println(ExcelCode.getStringData(4,0));
+		System.out.println(ExcelCode.getIntegerData(4, 1));
 		
 
 }
